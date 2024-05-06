25aad75bc707201d590d95c6255d22d383ff7a78
MirMir329 <emirhansamilov329@gmail.com>
2024-05-07 01:02:36 +0600

    Вторая версия проекта. Создал блок austranauts, куда выводятся отфильтрованные данные (объекты имеющие полные данные) ввиде картинок. Добавлена луна которая следует за центром экрана. Присутствует плохая (позже улучшенная), но работающая пагинация. Нормально работает переключатель блоков при нажатии на кнопку и header. Так же нет хорошего адаптива, ховера и курсора.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const austranavtsBtn = document.querySelector(".austranavts-btn");
+const austranavts = document.querySelector(".austranavts");
+const austranavtsCards = document.querySelector(".austranavts-cards");
+const div0 = document.querySelector(".div0");
 const insight = document.querySelector(".insight-background");
 const header = document.querySelector(".header");
 const movingPlanets = document.querySelector(".moving-planets");
@@ -11,9 +15,11 @@ const wordPACE = document.querySelector(".word-PACE");
 const charE = document.querySelector(".char-E");
 const charS = document.querySelector(".char-S");
 const universeSimulator = document.querySelector(".universe-simulator");
+const pagination = document.querySelector(".pagination");
 const historyBtn = document.querySelector(".history-btn");
+const pistureOfTheDayBtn = document.querySelector(".pisture-of-the-day-btn");
+const sectionPicofzday = document.querySelector(".section-picofzday");
 const logoBtn = document.querySelector(".logo-btn");
-const austranavtsBtn = document.querySelector(".austranavts-btn");
 
 setTimeout(() => {
     blueLight.classList.add("slow-appear");
@@ -38,11 +44,28 @@ setTimeout(() => {
 }, 7500);
 
 
+
+
+let count = 0;
+let spaceArr = [];
+
 const getDataAustranavts = async () => {
     const res = await fetch("https://data.nasa.gov/resource/9kcy-zwvn.json");
     if (res.ok) {
         const data = await res.json();
-        console.log(data);
+        if (count != 1) {
+            count++
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].eva != undefined & data[i].crew != undefined & data[i].date != undefined & data[i].duration != undefined & data[i].purpose != undefined & data[i].country != undefined & data[i].vehicle != undefined){
+                    spaceArr.push(data[i]);
+                    
+                }
+            }
+            makeSpaceCard(spaceArr);
+        }
+            
+        console.log(spaceArr);
+        createPadination(spaceArr)
     }
 }
 
@@ -54,6 +77,15 @@ const getDataHistory = async (e) => {
     }
 }
 
+const getDataPictureOfTheDay = async (e) => {
+    const res = await fetch("https://api.nasa.gov/planetary/apod?api_key=afyo01MR3snt2TcyrvPjA5UNTN0EhkXe3emGiXYH");
+    if(res.ok) {
+        const data = await res.json();
+        console.log(data);
+        localStorage.setItem("data", JSON.stringify(data))
+        makePictureOfTheDay()
+    }
+}
 
 const toggleElemHide = (...elems) => {
     elems.forEach(item => item.classList.toggle("hide"));
@@ -65,11 +97,144 @@ const toggleElemBG = (elem) => {
 }
 
 const showFrontPage = () =>{
-    toggleElemBG(header);
+    if (!austranavts.classList.contains("hide")) {
+        toggleElemBG(header);
+        toggleElemHide(insight, austranavts, pagination);
+    } else if (austranavts.classList.contains("hide")) {
+    }
+}
 
+const showSpaceCard = () => {
+    if (!insight.classList.contains("hide")) {
+        toggleElemBG(header);
+        toggleElemHide(insight, austranavts, pagination);
+    } else if (insight.classList.contains("hide")) {
+
+    }
+}
+
+const makeSpaceCard = (data) => {
+    showSpaceCard()
+    
+    austranavtsCards.innerHTML = `
+            <div class="div0">
+                <div class="extra-div">
+
+                </div>
+            </div>`
+    let counter = 1;
+    for (let i = 1; i <= 10; i++) {
+        if (counter === 1) {
+            counter = 2;
+            const card = `
+            <div class="background${counter}">
+                <div class="card">
+                    <h3>Crew: ${data[i].crew}</h3>
+                    <span>Date: ${data[i].date}</span>
+                    <span>Duration: ${data[i].duration}</span>
+                    <span>Vehicle: ${data[i].vehicle}</span>
+                    <span class="purpose">Purpose: ${data[i].purpose}</span>
+                    <span>Country: ${data[i].country}</span>
+                </div>
+            </div>
+            `
+            austranavtsCards.insertAdjacentHTML("beforeend", card);
+            
+        } else if (counter === 2) {
+            counter = 1;
+            const card = `
+            <div class="background${counter}">
+                <div class="card">
+                    <h3>Crew: ${data[i].crew}</h3>
+                    <span>Date: ${data[i].date}</span>
+                    <span>Duration: ${data[i].duration}</span>
+                    <span>Vehicle: ${data[i].vehicle}</span>
+                    <span class="purpose">Purpose: ${data[i].purpose}</span>
+                    <span>Country: ${data[i].country}</span>
+                </div>
+            </div>
+            `
+            austranavtsCards.insertAdjacentHTML("beforeend", card);
+        }     
+    }
+}
+
+window.addEventListener("scroll", (e) => {
+    let scrollY = window.scrollY;
+    let movingPlanetsHeight = movingPlanets.offsetHeight;
+    if (scrollY >= movingPlanetsHeight) {
+        div0.classList.add("slow-appear");
+        div0.style.top = `${scrollY - movingPlanetsHeight + 150}px `;
+    } else {
+        div0.classList.add("slow-disappear");
+        div0.classList.remove("slow-appear");
+    }
+})
+
+
+const createPadination = () => {
+    pagination.innerHTML = ""
+    const buttonsCount = Math.ceil(spaceArr.length / 10);
+    console.log(buttonsCount);
+
+    for (let i = 1; i < buttonsCount; i++) {
+        const button = document.createElement("button");
+        button.textContent = i;
+        pagination.append(button);
+        
+    }
+}
+
+const openProductsPage = async (e) => {
+    const paginationButton = e.target.closest("button");
+
+    if (paginationButton) {
+        austranavtsCards.innerHTML = ""
+        const currentPage = paginationButton.textContent;
+        let skip = 10 * currentPage;
+
+        const arr = spaceArr;
+        const newArr = arr.slice(skip, skip + 10)
+        console.log(newArr);
+        let counter = 1;
+        for (let i = 0; i < newArr.length; i++) { 
+            if (counter === 1) {
+                counter = 2;
+                const card = `
+                <div class="background${counter}">
+                    <div class="card">
+                        <h3>Crew: ${newArr[i].crew}</h3>
+                        <span>Date: ${newArr[i].date}</span>
+                        <span>Duration: ${newArr[i].duration}</span>
+                        <span class="purpose">Purpose: ${newArr[i].purpose}</span>
+                        <span>Country: ${newArr[i].country}</span>
+                    </div>
+                </div>
+                `
+                austranavtsCards.insertAdjacentHTML("beforeend", card);
+            } else if (counter === 2) {
+                counter = 1;
+                const card = `
+                <div class="background${counter}">
+                    <div class="card">
+                        <h3>Crew: ${newArr[i].crew}</h3>
+                        <span>Date: ${newArr[i].date}</span>
+                        <span>Duration: ${newArr[i].duration}</span>
+                        <span class="purpose">Purpose: ${newArr[i].purpose}</span>
+                        <span>Country: ${newArr[i].country}</span>
+                    </div>
+                </div>
+                `
+                austranavtsCards.insertAdjacentHTML("beforeend", card);
+            }     
+        }
+    } else {
+        console.log("Hello");
+    }
 }
 
 
 logoBtn.addEventListener("click", showFrontPage)
 austranavtsBtn.addEventListener("click", getDataAustranavts);
+pagination.addEventListener("click", openProductsPage);
 historyBtn.addEventListener("click", getDataHistory);
